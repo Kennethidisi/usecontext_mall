5338b4f4e610397136c8b7a838fd24d6dcee6c5f
Kennethidisi <127211509+Kennethidisi@users.noreply.github.com>
2023-06-20 12:51:11 +0100

    Add files via upload

---
+++ src/components/Cart.js
@@ -0,0 +1,39 @@
+import { useContext} from "react"
+import {useState, useEffect } from "react"
+import { cartContext } from "../Context"
+import Product from "./Product"
+import Header from "./Header"
+
+
+export default function Carts(){
+    const {cart, setCart} = useContext(cartContext)
+
+    const [totalPrice, setTotalPrice] = useState()
+     
+      useEffect(()=>{
+             
+             setTotalPrice(
+                 cart.reduce((total, item)=>{
+                          return total + Number(item.price)
+                   },0)
+             )
+      }, [cart])
+      
+    if(cart.length === 0){
+      return <h1>No Carts Available</h1>
+    }
+
+    return(
+        <div className="container">
+
+               <Header/>
+              <div className="cartContainer">
+                <h1>Total Price: $<b>{totalPrice}</b></h1>
+                {cart.map((prod)=>{
+                 return <Product prod={prod}/>
+           })}
+          </div>
+
+        </div>
+    )
+}
